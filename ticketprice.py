5dd64b8e4c736c55d145a13fdcce674bcf8f4f6b
alikghrabi <alikghrabi@gmail.com>
2025-02-16 01:17:34 +0200

    Add movie ticket to clients depending on their ages.

--- ticketprice.py
+++ ticketprice.py
@@ -1,2 +1,13 @@
 # Scaning age of Cliemt to get his price
 client_age = int(input("Enter your age: "))
+
+if client_age < 5:
+    print("The ticet is free!!!")
+elif client_age >=5 and client_age < 10:
+    print("The ticket price is 5.0 $")
+elif client_age >=10 and client_age < 18:
+    print("The ticket price is 6.0 $")
+elif client_age >= 18 and client_age < 27:
+    print("The ticket price is 7.0 $")
+else:
+    print("The ticket price is 10.0 $")
