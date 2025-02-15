b48cc02b9426fb07f14840b6b633c4d25a364bb6
alikghrabi <alikghrabi@gmail.com>
2025-02-16 01:29:52 +0200

    Add basic username and password  authentication check

---
+++ loginsys.py
@@ -0,0 +1,7 @@
+usrname = input("Enter your username: ")
+passwrd = input("Enter your password: ")
+
+if usrname == "admin" and passwrd == "alikghrabi123@#":
+    print("Welcome " + usrname)
+else:
+    print("Access Denied!!!!!")
